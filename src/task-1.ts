6d6e30c2607c356c603c1c0066f0dfa3e507cd8a
Alina Y. <devadevam.tod@gmail.com>
2025-09-30 18:25:11 +0300

    finish

---
+++ src/task-1.ts
@@ -0,0 +1,8 @@
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log('///////// task 1 /////////');
+console.log(`Name: ${name}`,"Type:", typeof(name));
+console.log(`Age: ${age}`,"Type:", typeof(age));
+console.log(`Online: ${isOnline}`,"Type:", typeof(isOnline));
